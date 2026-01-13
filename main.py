6bff5201c6e833de3dd8c90eefc306e82d9dc6d0
SeraPhim <fidafathima803@gmail.com>
2026-01-13 20:35:08 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,47 +7,46 @@ def show_menu():
 
 def load_tasks():
   try:
-    with open("tasks.txt", "r") as
-file:
-        return
+    with open("tasks.txt", "r") as file:
+      return
 file.read().splitlines()
-    except:
-         return []
+   except:
+      return []
 
 def save_tasks(tasks):
   with open("tasks.txt", "w") as file:
-        for task in tasks:
-          file.write(task + "\n")
+    for task in tasks:
+      file.write(task + "\n")
 
 def main():
   tasks = load_tasks()
-
-   while True:
-     show_menu()
-     choice = input("Choose an option:")
-
-if choice == "1":
-  print("\nYour tasks:")
-  for i, task in enumerate(tasks, 1):
-    print(f"{i}. {task}")
-
-elif choice =="2":
-    task = input("Enter new task:")
-    tasks.append(task)
-    save_tasks(tasks)
-    print("Task added!")
-
-elif choice =="3":
-    for i, task in enumerate(tasks, 1):
-      print(f"{i} {task}")
-      num = int(input("Enter task number to delete:"))
-      tasks.pop(num - 1)
-      save_tasks(tasks)
-      prinr("Task deleted!")
-
-elif choice == "4":
-     print("Goodbye!")
-     break
-
-else:
-    print("Invalid choice!")
+  
+    while True:
+      show_menu()
+      choice = input("Choose an option: ")
+      
+        if choice == "1":
+          print("\nYour tasks:")
+          for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
+            
+        elif choice == "2":
+          task = input("Enter new task: ")
+          tasks.append(task)
+          save_tasks(tasks)
+          print("Task added!")
+          
+        elif choice == "3":
+          for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
+          num = int(input("Enter task number to delete: "))
+          tasks.pop(num - 1)
+          save_tasks(tasks)
+          print("Task deleted!")
+          
+        elif choice == "4":
+          print("Goodbye!")
+          break
+          
+        else:
+          print("Invalid choice!")
